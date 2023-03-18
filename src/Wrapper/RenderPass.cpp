3d7fd57ae388ab27272b04f7bf0d7d50ad997802
MoCheng <mochenghh@gmail.com>
2023-03-18 14:26:06 +0800

    🔧 fix vertex buffer format

--- src/Wrapper/RenderPass.cpp
+++ src/Wrapper/RenderPass.cpp
@@ -6,36 +6,7 @@ namespace MoCheng3D {
 RenderPass::RenderPass()
 {
 
-    // vk::AttachmentDescription attach_des;
-    // auto format = Get_Context_Singleton().Get_SwapChain()->Get_Format();
-    // attach_des.setFormat(format)
-    //     .setInitialLayout(vk::ImageLayout::eUndefined)
-    //     .setFinalLayout(vk::ImageLayout::ePresentSrcKHR)
-    //     .setLoadOp(vk::AttachmentLoadOp::eClear)
-    //     .setStoreOp(vk::AttachmentStoreOp::eStore)
-    //     .setStencilLoadOp(vk::AttachmentLoadOp::eDontCare)
-    //     .setStencilStoreOp(vk::AttachmentStoreOp::eDontCare)
-    //     .setSamples(vk::SampleCountFlagBits::e1);
-
-    // vk::AttachmentReference attach_reference;
-    // attach_reference.setAttachment(0).setLayout(
-    //     vk::ImageLayout::eColorAttachmentOptimal);
-
-    // subpass.setPipelineBindPoint(vk::PipelineBindPoint::eGraphics)
-    //     .setColorAttachments(attach_reference);
-    // vk::RenderPassCreateInfo create_info;
-    // vk::SubpassDependency subpass_dependency;
-    // subpass_dependency.setSrcSubpass(VK_SUBPASS_EXTERNAL)
-    //     .setDstSubpass(0)
-    //     .setDstAccessMask(vk::AccessFlagBits::eColorAttachmentWrite | vk::AccessFlagBits::eColorAttachmentRead)
-    //     .setSrcStageMask(vk::PipelineStageFlagBits::eColorAttachmentOutput)
-    //     .setDstStageMask(vk::PipelineStageFlagBits::eColorAttachmentOutput);
-
-    // create_info.setAttachments(attachment_descriptions);
-    // create_info.setSubpasses(subpass);
-    // create_info.setDependencies(subpass_dependency);
-    // m_handle = Get_Context_Singleton().Get_Device()->Get_handle().createRenderPass(
-    //     create_info);
+    
 }
 
 void RenderPass::Add_Attachment_description(vk::AttachmentDescription attach_des )
